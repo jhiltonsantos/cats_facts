e619db8eb38a0ebef4975c1a9d1481a0e5a72ec1
jhiltonsantos <jhilton930@gmail.com>
2024-06-05 12:39:43 -0300

    Add(component): add components

---
+++ components/TextFact/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <div v-if="isLoading">
+      <p class="animate-fadeIn">Loading...</p>
+    </div>
+
+    <div :key="factKey" v-else-if="fact">
+      <ul>
+        <li>
+          <p class="animate-fadeIn">{{ fact }}</p>
+        </li>
+      </ul>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+const props = defineProps({
+  factKey: Number,
+  isLoading: Boolean,
+  fact: String
+})
+</script>
+
+<style scoped>
+p {
+  margin: 20px 0;
+  @apply text-xl font-normal
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  @apply transition-opacity duration-500;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  @apply opacity-0;
+}
+</style>
